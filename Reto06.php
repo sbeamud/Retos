7ec4c9d69bd7472c756d2aeaf581e9ade46e949f
sbeamud <sergiobeamud@iesrioarba.es>
2025-01-12 06:41:01 +0100

    Se incluye Reto06

---
+++ Reto06.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Crea una función que imprima los 30 próximos años bisiestos siguientes a un año dado.
+ - Utiliza el menor número de líneas para resolver el ejercicio.
+*/
+
+/*
+function calcula_bisiestos($anio) {
+    $bisiestos = [];
+    while (count($bisiestos) < 30) { 
+        if (($anio % 4 === 0 && ($anio % 100 !== 0) || ($anio % 400 === 0))) { 
+            $bisiestos[] = $anio; 
+        }
+        $anio++; 
+    }
+
+    foreach ($bisiestos as $bisiesto) {
+        echo $bisiesto . "<br>";
+    }
+}
+calcula_bisiestos(date("Y"));
+*/
+
+
+
+?>
+
+
